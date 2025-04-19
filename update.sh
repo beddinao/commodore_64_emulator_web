be6429bc0b55f6b7d8962058324e77494c9dcea9
beddinao <bilaleddinaoui@gmail.com>
2025-04-19 17:12:23 +0100

    update update.sh

---
+++ update.sh
@@ -0,0 +1,33 @@
+#/usr/bin/bash
+
+TARGET_REPO=https://github.com/beddinao/MetallC64
+TARGET_NAME=MetallC64
+EMSDK_REPO=https://github.com/emscripten-core/emsdk
+EMSDK_NAME=emsdk
+PARENT_DIR=`pwd`
+
+mkdir compile-env && cd compile-env
+git clone $EMSDK_REPO && cd $EMSDK_NAME
+CUR_PWD=`pwd`
+
+$CUR_PWD/emsdk install latest
+$CUR_PWD/emsdk activate latest
+source emsdk_env.sh
+
+cd ..
+git clone $TARGET_REPO && cd $TARGET_NAME
+git switch emscripten_ready
+CUR_PWD=`pwd`
+
+cd assets/SDL3
+mkdir build && cd build
+
+emcmake cmake .. && emmake make -j13
+cd $CUR_PWD
+emmake make
+
+cd $PARENT_DIR 
+mv compile-env/MetallC64/MetallC64.wasm compile-env/MetallC64/MetallC64.js assets
+git add assets && git commit -m "update" && git push
+
+
